18765a90795ca3852d349e313d435412479c3675
Michael Trachtenberg <mike.trachtenberg@gmail.com>
2019-08-24 20:06:24 -0400

    Bug Juice

---
+++ app/Http/Requests/DirectorRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DirectorRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        $rules = [
+            'name'  => 'string|min:1|max:150|unique:directors',
+        ];
+
+        if ($this->isMethod('post')) {                         // adding new movie
+            foreach ($rules as & $rule) {
+                $rule .= '|required';
+            }
+            unset($rule);
+        } else if ($this->isMethod('patch')) {                 // updating movie
+            if ($this->request->has('name')) {
+                $rules['name'] .= ',name'. $this->get('id');;
+            }
+        }
+
+        return $rules;
+    }
+}
--- database/migrations/2019_08_22_202509_create_director_movie_table.php
+++ database/migrations/2019_08_22_202509_create_director_movie_table.php
@@ -28,6 +28,6 @@ class CreateDirectorMovieTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('directors_movies');
+        Schema::dropIfExists('director_movie');
     }
 }
